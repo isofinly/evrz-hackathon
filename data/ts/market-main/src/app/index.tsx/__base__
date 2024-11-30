5c9996e9fc9b8bf5bb35302924000466936047ef
zen0no <zeenoonoo@gmail.com>
2024-11-30 21:09:08 +0300

    Fix chunking

--- data/ts/market-main/src/app/index.tsx/__base__
+++
@@ -1,6 +0,0 @@
-import App from "./app";
-import { withProviders } from "./providers";
-
-const MyApp = withProviders(App);
-
-export default MyApp;
---
+++ review_backend/chunk_data.py
@@ -0,0 +1,34 @@
+from src.parsers.parser import parse_file
+from src.parsers.language import LANGUAGE
+from argparse import ArgumentParser
+
+from pathlib import Path
+
+parser = ArgumentParser()
+parser.add_argument("-i", "--input", type=str, help="Path to file or directory with files", required=True)
+parser.add_argument("-o", "--output", type=str, help="Path to output file", required=True)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    path = Path(args.input).resolve()
+    output_path = Path(args.output).resolve()
+
+    for file_path in path.rglob("*"):
+        if file_path.suffix[1:] not in LANGUAGE:
+            continue
+        file_path = file_path.relative_to(path)
+        
+        base_chunk, declarations = parse_file(path / file_path)
+
+        out_file_path = output_path / file_path
+        
+        out_base_path = out_file_path / "__base__.json"
+        out_base_path.parent.mkdir(parents=True, exist_ok=True)
+        base_chunk.to_json(out_base_path)
+
+        for identifier, declaration in declarations.items():
+            out_identifier_path = out_file_path / f"{identifier}.json"
+            out_identifier_path.parent.mkdir(parents=True, exist_ok=True)
+            declaration.to_json(out_identifier_path)
